5d8021bb572339057385e2603513e4dd87626c7c
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-28 17:01:23 +0000

    :)

--- layout_top.php
+++ layout_top.php
@@ -19,68 +19,8 @@ require_once 'config.php';
 	<br />
 
 	<div id="wrapper">
-		<?php require_once 'header.php'; ?>
-
-		<div id="leftcolumnwrap">
-			<div id="leftcolumn">
-				<div class="title">
-					<img
-						src="/arrow.gif" />
-					tinket :)
-				</div>
-				<center>
-					<a href="/trinket">
-						<img src="/trinket/Trinket comm cropped.png" style="height: 200px" />
-					</a>
-				</center>
-			</div>
-
-			<div id="leftcolumn">
-				<div class="title">
-					<img
-						src="/arrow.gif" />
-					webrings
-				</div>
-				<div id="vocaring" class="small"></div>
-				<link rel="stylesheet" href="https://electric-tenshi.nekoweb.org/vocaring/vocaring.css">
-				<script src="https://electric-tenshi.nekoweb.org/vocaring/vocaring-variables.js"></script>
-				<script src="https://electric-tenshi.nekoweb.org/vocaring/vocaring-widget.js"></script>
-				<br />
-				<br />
-			</div>
-
-			<div id="leftcolumn">
-				<div class="title">
-					<img
-						src="/arrow.gif" />
-					MY SUPER COOL FRIENDS
-				</div>
-				<a href="https://www.najah.cloud/"><img src="/najahbutton.png"></img></a>
-				<br />
-				<br />
-			</div>
-
-			<div id="leftcolumn">
-				<div class="title">
-					<img
-						src="/arrow.gif" />
-					blinkies
-				</div>
-
-				<center>
-					<?php
-					$absolutePath = BASE_PATH . 'blinkies/';
-					$blinkiesDir = glob($absolutePath . "*.{jpg,png,gif}", GLOB_BRACE);
-
-					foreach ($blinkiesDir as $blinky) {
-						// Convert server path back to a web-friendly relative URL for the <img> tag
-						$webPath = '/blinkies/' . basename($blinky);
-						echo '<img src="' . htmlspecialchars($webPath) . '" alt="blinky"/>';
-					}
-					?>
-				</center>
-			</div>
-		</div>
+		<?php require_once '/header.php'; ?>
+		<?php require_once '/sidebar.php'; ?>
 
 <div id="contentwrap">
 	<div id="content">
