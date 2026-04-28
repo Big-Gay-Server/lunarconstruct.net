c8a486de318b8a2b87ca2873b081433d31731cd9
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-28 21:57:24 +0000

    layout planning

---
+++ layoutplan.php
@@ -0,0 +1,15 @@
+this is what we have now, on the nginx container's end at least. ish
+
+usr/
+├─ share/
+│  ├─ nginx/
+│  │  ├─ html/
+│  │  │  ├─ main/
+│  │  │  ├─ vocalsynth/
+│  │  │  │  ├─ includes/
+│  │  │  │  │  ├─ functions.php
+│  │  │  │  │  ├─ covermanager.php
+│  │  │  │  │  ├─ voicebankmanager.php
+│  │  │  ├─ lunatine/
+
+but i wanna have the functions and shared assets all together and be able to access everything together
