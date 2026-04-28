eb4faf15c85e3d683e95257b57dda718e9c2226b
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-28 22:04:33 +0000

    layoutttt

--- layoutplan.php
+++ layoutplan.php
@@ -12,4 +12,6 @@ usr/
 │  │  │  │  │  ├─ voicebankmanager.php
 │  │  │  ├─ lunatine/
 
-but i wanna have the functions and shared assets all together and be able to access everything together
+but i wanna have the functions and shared assets all together and be able to access everything together. might even get rid of the current path alltogether
+
+idk the main thing i need to figure out is how it is all set up on my host system not the docker. docker is easily configurable to what i need
