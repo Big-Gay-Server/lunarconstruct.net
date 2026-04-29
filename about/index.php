12a149a5ee3942aa59ad3cc9f58b6bdf2d3d0bea
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-29 18:13:41 +0000

    does that change anything

--- about/index.php
+++ about/index.php
@@ -1,11 +1,7 @@
 <div class="mainsection">
 <p>hello! i'm really bad at writing about myself so sorry in advance ^^;
-
 <p>my name is lunar skye, i go by LunarConstruct online ! i'm <span id="age"></span> years old, and currently living in new jersey!
-
 <p>i work a kinda boring desk job, so recently i've started getting into selfhosting! it keeps me entertained enough haha i self host this site as well as my vocalsynth site and my lunatine (worldbuilding) site, and i've been trying to learn lots of things along the way c:
-
 <p>i'm not as active as i used to be in online communities, but thats okay !! vocalsynth stuff was kind of my main thing for a long time, but i'm glad i'm starting to spread out and make my own space on the web !
-
 <p>anyway, thank you for checking out my little garden here!! if you're reading this you're very cool and very swag x3
 </div>
