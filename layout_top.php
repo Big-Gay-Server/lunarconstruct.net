d97f68af1624bd094f9ab0370717ed4f5ff7db11
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-29 21:15:16 +0000

    huh

--- layout_top.php
+++ layout_top.php
@@ -10,7 +10,7 @@ require_once 'config.php';
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title>LunarConstruct</title>
 	<link href="/style.css" rel="stylesheet" />
-	<link href="/_assets/fonts/fonts.css" rel="stylesheet" />
+	<link href="/_assets/fonts/fonts.css?v=2" rel="stylesheet" />
 	<link rel="icon" type="image/x-icon" href="/_assets/favicon.ico">
 </head>
 
